517d777c59c4c49f447a25d3ac1483a013463c8d
doqendev <210837548+doqendev@users.noreply.github.com>
2025-06-09 10:54:53 +0100

    asdasdas
    asdasd

--- eslint.config.js
+++ eslint.config.js
@@ -5,6 +5,25 @@ import reactRefresh from 'eslint-plugin-react-refresh'
 
 export default [
   { ignores: ['dist'] },
+
+  // For Node scripts and functions
+  {
+    files: ['functions/**/*.{js,jsx,ts,tsx}', 'scripts/**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      globals: { ...globals.node, ...globals.es2021 }, // add node globals
+      parserOptions: {
+        ecmaVersion: 'latest',
+        sourceType: 'module',
+      },
+    },
+    rules: {
+      ...js.configs.recommended.rules,
+      // Optionally, any Node-specific rules
+    },
+  },
+
+  // For React/browser code
   {
     files: ['**/*.{js,jsx}'],
     languageOptions: {
